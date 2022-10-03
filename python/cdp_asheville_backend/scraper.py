6e86c452584cc056cc7ea54e741ef03bfbdfd1e7
Patrick Conant <patrick@prcapps.com>
2022-10-02 20:07:20 -0400

    Temp disable captions

--- python/cdp_asheville_backend/scraper.py
+++ python/cdp_asheville_backend/scraper.py
@@ -625,8 +625,8 @@ class AshevilleScraper(IngestionModelScraper):
         uri: str,
         **kwargs,
     ) -> Optional[str]:
-        # print("Captions disabled")
-        # return None
+        print("Captions disabled")
+        return None
 
         print("Download Subtitle: " + uri)
 
